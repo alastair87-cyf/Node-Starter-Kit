96a188e3d080de392c7c2f9d5129460eb10fd986
Alastair Cooper <alastair.cooper@outlook.com>
2020-09-26 11:49:10 +0100

    made port dynamic and added Hello World response

--- server.js
+++ server.js
@@ -3,4 +3,4 @@ const app = express();
 
 app.get("/", (req, res) => console.log("GET /"));
 
-app.listen(3000, () => console.log("Server is listening on port 3000. Ready to accept requests!"));
+app.listen(process.env.PORT, () => console.log("Server is listening on port 3000. Ready to accept requests!"));
